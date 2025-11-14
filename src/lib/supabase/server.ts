3f889a4351f5be8e9ed66b4eb18e2531a4a62609
BroonTreyer <129778665+BroonTreyer@users.noreply.github.com>
2025-11-13 21:47:38 -0300

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,18 +1,18 @@
-import { cookies } from "next/headers";
-import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers"
+import { createServerClient } from "@supabase/ssr"
 
-export function createClient() {
-  const cookieStore = cookies();
+export const createClient = () => {
+  const store = cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
-          return cookieStore.get(name)?.value;
-        },
-      },
+        get(name: string) {
+          return store.get(name)?.value
+        }
+      }
     }
-  );
+  )
 }

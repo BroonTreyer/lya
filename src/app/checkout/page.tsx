b6b62d09e9e53e3ea26658f12491eeed1715549f
BroonTreyer <129778665+BroonTreyer@users.noreply.github.com>
2025-11-13 22:58:47 -0300

    Update page.tsx

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,13 +1,12 @@
-"use client"
+"use client";
 
-import { Suspense } from "react"
-
-import CheckoutContent from "./content"
+import { Suspense } from "react";
+import CheckoutContent from "./CheckoutContent";
 
 export default function CheckoutPage() {
   return (
     <Suspense fallback={<>Carregando...</>}>
       <CheckoutContent />
     </Suspense>
-  )
+  );
 }
